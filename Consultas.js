2437005357a582f8475051af3a7003a6446fd4f9
Cesar Cesconetto <cesar.cesconetto@gmail.com>
2018-08-08 18:50:29 -0300

    Add files via upload

--- Consultas.js
+++ Consultas.js
@@ -22,7 +22,7 @@ db.Medico.find( { "endereço.estado": { $in: [ "California" ] } } )
 // Consultas que tenham data superior ou igual a 16-05-2018, exibindo o paciente e o médico:
 db.Consulta.find( { data: { $gte: "2018-05-16" } }, { _id: 0, paciente: 1, medico: 1 } )
 // Pacientes que não possuem email, exibindo o nome e o CPF:
-db.Paciente.find( { emails: { $exists: false } }, { _id: 0, nome: 1, cpf: 1 } )
+db.Paciente.find( { email(s): { $exists: false } }, { _id: 0, nome: 1, cpf: 1 } )
 // Pacientes menores de 18 anos:
 db.Paciente.find(
   { dataNasc: { $gt: "2000-01-01" } },
@@ -33,13 +33,13 @@ db.Paciente.find(
 // (01) Consulta com regex:
 
 // Médico que tenha "Adam" no nome e possua um email, exibindo o nome e o CRM:
-db.Medico.find( { nome: /Adam/, "emails.0": { $exists: true } }, { _id: 0, nome: 1, crm: 1} )
+db.Medico.find( { nome: /Adam/, "email(s).0": { $exists: true } }, { _id: 0, nome: 1, crm: 1} )
 
 
 // (01) Consulta com acesso a array:
 
 // Nome e telefone de todos os Médicos:
-db.Medico.find( { }, { _id: 0, nome: 1, telefones: 1 } )
+db.Medico.find( { }, { _id: 0, nome: 1, telefone(s): 1 } )
 
 
 // (01) Consulta com acesso à estrutura embutida:
